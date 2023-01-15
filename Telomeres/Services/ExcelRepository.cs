2494a685c2c45827311b54105966d95d3630f0f1
Dario Garcia <dario61081@gmail.com>
2023-01-15 18:17:08 -0300

    actualizado view de reports

---
+++ Telomeres/Services/ExcelRepository.cs
@@ -0,0 +1,45 @@
+﻿using FastExcel;
+
+namespace Telomeres.Services
+{
+
+
+    public class ExcelRepository
+    {
+
+        private const string TAB_DATA_SOURCE = "Tabular Data";
+        private const string EXCEL_DATA_SOURCE = "Libro1.xlsx";
+        private string FilePath;
+
+
+        public ExcelRepository(string filepath)
+        {
+            this.FilePath = filepath;
+        }
+
+        public string ReadFile()
+        {
+
+            /*
+            *  1. open excel data source
+            *  2. read "tab data"
+            *  
+            */
+
+
+            var inputFile = new FileInfo(FilePath);
+            Worksheet worksheet = new Worksheet();
+            using (FastExcel.FastExcel fastExcel = new FastExcel.FastExcel(inputFile, true))
+            {
+                worksheet = fastExcel.Read(TAB_DATA_SOURCE);
+
+                var rows = worksheet.Rows;
+
+
+            }
+
+
+            return "";
+        }
+    }
+}
